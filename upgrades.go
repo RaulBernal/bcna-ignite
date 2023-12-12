a9f8b2dad15e0d94974b485523be865e6d32bf1e
Raul Bernal <bernal.raul@gmail.com>
2023-12-12 12:31:17 +0100

    fix upgrade handler

---
+++ upgrades.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	storetypes "cosmossdk.io/store/types"
+	circuittypes "cosmossdk.io/x/circuit/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+)
+
+// RegisterUpgradeHandlers registers upgrade handlers.
+
+func (app App) RegisterUpgradeHandlers() {
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	app.StickyFingers(upgradeInfo)
+}
+func (app *App) StickyFingers(_ upgradetypes.Plan) {
+	planName := "stickyfingers"
+	app.UpgradeKeeper.SetUpgradeHandler(
+		planName,
+		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			//ctx.Logger().Info("Cosmos-SDK v0.50.x is here...")
+
+			return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+		},
+	)
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == planName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{
+				circuittypes.ModuleName,
+			},
+		}
+
+		// configure store loader that checks if version == upgradeHeight and applies store upgrades
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+
+}
